d6f19dc2d4fca7e0ec20f7f9686681d811e33045
Pablo Apablaza <papablaza@student.unimelb.edu.au>
2025-12-07 08:39:47 +1100

    Build for scrapper

--- src/PISPscrappers.jl
+++ src/PISPscrappers.jl
@@ -1,3 +1,4 @@
 include("scrappers/PISP-scrapper-utils.jl")
 include("scrappers/PISP-scrapper-2024traces.jl")
 include("scrappers/PISP-scrapper-2024files.jl")
+include("scrappers/PISP-scrapper-build.jl")
